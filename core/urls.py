d3006ec37b3cd3c9bf1454b9de0237eea18ed312
parimi3333 <parimi3333@gmail.com>
2023-06-21 00:27:30 +0530

    updated urls

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from core.views import (
-    NoteListCreateView,
-    NoteRetrieveUpdateDestroyView,
-    AudioNoteCreateView,
-    VideoNoteCreateView
-)
+from . import views
 
 urlpatterns = [
-    path('notes/', NoteListCreateView.as_view(), name='note-list-create'),
-    path('notes/<int:pk>/', NoteRetrieveUpdateDestroyView.as_view(), name='note-retrieve-update-destroy'),
-    path('audio-notes/', AudioNoteCreateView.as_view(), name='audio-note-create'),
-    path('video-notes/', VideoNoteCreateView.as_view(), name='video-note-create'),
+    path('notes/', views.note_list_create_view, name='note-list-create'),
+    path('notes/<int:pk>/', views.note_retrieve_update_destroy_view, name='note-retrieve-update-destroy'),
+    path('audio-notes/', views.audio_note_create_view, name='audio-note-create'),
+    path('video-notes/', views.video_note_create_view, name='video-note-create'),
 ]
